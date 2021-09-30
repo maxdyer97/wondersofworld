697e6fec983af00a99fc4af6fe8adc79cea256cc
Max <maximusdyer97@gmail.com>
2021-09-30 13:52:33 +0100

    changed newWonders.php file

--- newWonders.php
+++ newWonders.php
@@ -3,14 +3,6 @@
 require_once('includes/dbconnect.php');
 require_once('includes/functions.php');
 
-//if (!isValid) {
-//    header('Name: newWonders.php');
-//}
-
-//if (!isset($_POST['name']) || !$_POST['name']) {
-//    header('Location: index.php');
-//}
-
 $wonderAdded = addNewWonder($db, $_POST);
 
 if ($wonderAdded) {
